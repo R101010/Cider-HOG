c68de1d310e045e2e411c5023617650b7d6947ef
RR <RubenR42@icloud.com>
2020-03-04 04:20:39 -0800

    update yeasts controller

---
+++ controllers/yeasts.js
@@ -0,0 +1,35 @@
+var Yeast = require('../models/yeast');
+var Cider = require('../models/cider');
+
+module.exports = {
+    new: newYeast,
+    create,
+    addToBrew
+};
+
+function addToBrew(req, res) {
+    Cider.findById(req.params.id, function(err, cider) {
+        cider.yeast.push(req.body.yeastId);
+        cider.save(function(err) {
+            res.redirect(`/ciders/${cider._id}`);
+        });
+    });
+}
+
+function create(req, res) {
+    const s = req.body.bDay;
+    req.body.bDay =
+     `${s.substr(5,2)}-${s.substr(8,2)}-${s.substr(0,4)}`;
+  Yeast.create(req.body, function(err, yeast) {
+    res.redirect('/yeasts/new');
+});
+}
+
+function newYeast(req, res) {
+    Yeast.find({}, function(err, yeasts) {
+        res.render('yeasts/new', {
+            title: 'Add Yeast',
+            performers
+        });
+    });
+}
