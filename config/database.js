259eaf2f7ea6f5dedbac64c1b0e80dfa1ec88462
RR <RubenR42@icloud.com>
2020-03-04 23:34:39 -0800

    connect database

--- config/database.js
+++ config/database.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/ciders', {
-  useNewUrlParser: true,
-  useCreateIndex: true }
-);
-// mongoose.connect(process.env.DATABASE_URL, {
+// mongoose.connect('mongodb://localhost/ciders', {
 //   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true
-// });
+//   useCreateIndex: true }
+// );
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 
 // shortcut to mongoose.connection object
 const db = mongoose.connection;
 
 db.on('connected', function () {
+
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
 
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var passport = require('passport');
+// var methodOverride = require('method-override');
 
 require('dotenv').config();
 
@@ -27,9 +28,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(session({
-  secret: 'liquorIsQuicker',
+  secret: 'liquorIsQuicker!',
   resave: false,
   saveUninitialized: true
 }));
